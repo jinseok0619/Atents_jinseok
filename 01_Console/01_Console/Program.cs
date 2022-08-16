272a413a0992b63c070e8c607f5948bbde906e70
jinseok0619 <chlwlstjr159@gmail.com>
2022-08-16 14:59:14 +0900

    Console 실습파일 생성
    실습파일 생성

--- 01_Console/01_Console/Program.cs
+++ 01_Console/01_Console/Program.cs
@@ -10,6 +10,12 @@ namespace _01_Console
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Hello World");   // "Hello World"를 출력하는 코드
+            //Console.ReadKey();                  // 키 입력 대기하는 코드
+            //8/16
+            //자기 이름 출력하기
+            Console.WriteLine("제이름은 최진석 입니다"); 
+            Console.ReadKey();
         }
     }
 }
